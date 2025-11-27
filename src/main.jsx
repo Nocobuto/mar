a7d66f1cf2e47686a611551c2f0adc69d00e0344
Angel Bracamontes <angibc35@gmail.com>
2025-11-27 16:58:21 -0600

    primer commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
+
+// 👇 Importa tu imagen desde src
+import bg from './assets/bg.jpg'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter basename="/ci">
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
+
+// 👇 Aplica el fondo dinámicamente
+document.body.style.backgroundImage = `url(${bg})`
+document.body.style.backgroundSize = 'cover'
+document.body.style.backgroundRepeat = 'no-repeat'
+document.body.style.backgroundAttachment = 'fixed'
+document.body.style.backgroundPosition = 'center'
