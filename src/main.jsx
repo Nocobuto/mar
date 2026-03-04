529ae61ba492546595d148f5c9401b4680f6e504
Angel Bracamontes <angibc35@gmail.com>
2026-03-03 21:49:00 -0600

    fit: cambiar browserouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App.jsx'
 
-// 👇 Importa tu imagen desde src
 import bg from './assets/bg.jpg'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/ci">
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
 
-// 👇 Aplica el fondo dinámicamente
 document.body.style.backgroundImage = `url(${bg})`
 document.body.style.backgroundSize = 'cover'
 document.body.style.backgroundRepeat = 'no-repeat'
